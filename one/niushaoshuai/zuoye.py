de07d78a7aead048648dee2cfe114106895c9be5
niushaoshuai <>
2017-07-26 16:09:34 +0800

    commit task-1

--- one/niushaoshuai/zuoye.py
+++ one/niushaoshuai/zuoye.py
@@ -1,21 +1,47 @@
 #!/usr/bin/python
 #-!-  coding: UTF-8
-user_name=raw_input("please input your username: ")
-real_name="niushaoshuai"
-real_pass="12345678"
-count=0
-times=1
-if user_name==real_name :
-    while times<=3:
+
+def if_user_locked(u):
+    locked_user=open('user_locked.txt')
+    for name in locked_user.readlines():
+        name=name.strip('\n')
+        if u == name:
+            return 'locked'
+def if_login_ok(u,p):    
+    user_message=open('user_messages.txt')
+    for line in user_message.readlines():
+        user,passdb1=line.strip(' ').split()
+        if u==user and p==passdb1:
+            return 'True'
+        else:
+            return 'False'
+
+if __name__ == '__main__':
+    count=0
+    times=0
+    while times< 3:
+        user_name=raw_input("please input your username: ")
         user_pass=raw_input("please input your password: ")
         count=count+1
         times=times+1
-        if user_pass==real_pass:
-            print "welcom {}".format(real_name)
-            break
-        else:
-            print "input a error password,you have {} times".format(3-count)
+        user_status=if_user_locked(user_name)
+        if user_status=='locked':
+            print "user {} is locked".format(user_name)
+            exit(1)
+        if len(user_pass)<=6 :
+            print "password is too simple"
+            print "you have {} times left".format(3-times)
+        else :
+            if  if_login_ok(user_name,user_pass)!='True':
+                print "账号或密码不正确."
+                print "you have {} times left".format(3-times)
+            else:
+                print "login ok,welcom {}".format(user_name)
+                break
+
     else:
-        print "你的机会用完了,用户已被锁定 " 
-else:
-    print "user {} is not exist".format(user_name)
+        user_locked=open('user_locked.txt','a')
+        user_locked.write(user_name)
+        user_locked.write("\n")
+        user_locked.close()
+        print "你的机会用完了,用户已被锁定 "    
