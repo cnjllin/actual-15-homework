e5424ebea73e07e89986cdd47b5204f678d283f2
root <root@zk1.(none)>
2017-07-29 23:12:59 +0800

    zuoye commit:day:

---
+++ one/xuxiaopan/test.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python
+#coding:utf-8
+'''
+name = raw_input("please your name: ")
+if name == "wd":
+    pwd = raw_input("please your passwd: ")
+    if pwd == "123456":
+        print "welcome %s" % name
+   # elif pwd == "123":
+    #    print "你的密码位数太少" 
+    else:
+        print "Sorry,password is worng"
+else:
+    print "Sorry,%s is not exist" % name
+
+print "\033[035mhello world\033[0m"
+'''
+
+try:
+    1/0
+except Exception, e:
+    print "语法错误"        
