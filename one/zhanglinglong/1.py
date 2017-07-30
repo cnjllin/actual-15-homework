d945c8395ed2103f88fc0d2be18b4c551b618a3e
linglong0820 <zhanglinglong@163.com>
2017-07-30 10:44:43 +0800

    优化脚本,修改注释

--- one/zhanglinglong/1.py
+++ one/zhanglinglong/1.py
@@ -1,20 +1,20 @@
 #/usr/bin/python
 #coding:utf-8
-#输入错误，回车前可以清除
+# 输入错误，回车前可以清除
 import readline
 name = raw_input("请输入用户名：")
 if name == "zll":
     count = 0    
     while count <= 3:
         passwd = raw_input("请输入密码：")
-        #如果密码正确，打印欢迎信息
+        # 如果密码正确，打印欢迎信息
         if passwd == "ilikeit":
             print "欢迎%s登录" %name
-        #如果密码少于6位，直接退出
+        # 如果密码少于6位，直接退出
         if len(passwd) <6:
             print "密码位数必须超过6位"
             break
-        #如果密码输入错误，提示错误，并且最多可以输入3次，超过3次后退出
+        # 如果密码输入错误，提示错误，并且最多可以输入3次，超过3次后退出
         if count < 3:
             print "密码错误，你还有%s次机会" %(3 - count)
             count +=1
