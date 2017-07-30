fb2bf4bc62f11843e61d04e6fd03d30df661c7eb
linglong0820 <zhanglinglong@163.com>
2017-07-30 10:39:40 +0800

    优化脚本

--- one/zhanglinglong/1.py
+++ one/zhanglinglong/1.py
@@ -10,11 +10,11 @@ if name == "zll":
         #如果密码正确，打印欢迎信息
         if passwd == "ilikeit":
             print "欢迎%s登录" %name
-        #密码少于6位，直接退出
+        #如果密码少于6位，直接退出
         if len(passwd) <6:
             print "密码位数必须超过6位"
             break
-        #密码最多可以输入3次,3次退出后直接退出
+        #如果密码输入错误，提示错误，并且最多可以输入3次
         if count < 3:
             print "密码错误，你还有%s次机会" %(3 - count)
             count +=1
