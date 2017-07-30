6c1e70bf83c6e6a98b7370a917e36ef393c77fa0
linglong0820 <zhanglinglong@163.com>
2017-07-30 10:32:35 +0800

    优化脚本与导图

--- one/zhanglinglong/1.py
+++ one/zhanglinglong/1.py
@@ -6,18 +6,19 @@ if name == "zll":
     count = 0    
     while count <= 3:
         passwd = raw_input("请输入密码：")
+        #如果密码正确，打印欢迎信息
         if passwd == "ilikeit":
+            print "欢迎%s登录" %name
+        #密码少于6位，直接退出
+        if len(passwd) <6:
+            print "密码位数必须超过6位"
             break
+        #密码最多可以输入3次
+        if count < 3:
+            print "密码错误，你还有%s次机会" %(3 - count)
+            count +=1
         else:
-	    if len(passwd) <6:
-                print "密码位数必须超过6位"
-            else: 
-                if count < 3:
-                    print "密码错误，你还有%s次机会" %(3 - count)
-                    count +=1
-                else:
-                    print "输入次数过多"
-                    exit()
-    print "欢迎%s登录" %name
+            print "输入次数过多"
+            break
 else:
     print "账号%s不存在" %name
