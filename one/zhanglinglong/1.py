0832f7f556f471e9bb748381c5ccbf38a926af62
linglong0820 <zhanglinglong@163.com>
2017-07-30 11:12:41 +0800

    添加strip参数

--- one/zhanglinglong/1.py
+++ one/zhanglinglong/1.py
@@ -2,14 +2,16 @@
 #coding:utf-8
 # 输入错误，回车前可以清除
 import readline
-name = raw_input("请输入用户名：")
+name = raw_input("请输入用户名：").strip()
 if name == "zll":
     count = 0    
     while count <= 3:
         passwd = raw_input("请输入密码：")
+        passwd = passwd.strip()
         # 如果密码正确，打印欢迎信息
         if passwd == "ilikeit":
             print "欢迎%s登录" %name
+            break
         # 如果密码少于6位，直接退出
         if len(passwd) <6:
             print "密码位数必须超过6位"
