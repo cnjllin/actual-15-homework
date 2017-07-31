a70848cf51187e2365be926354291cc4b90a6868
greatestray <yutian163@126.com>
2017-07-31 16:54:55 -0400

    添加注释

--- two/qinlei/zuoye.py
+++ two/qinlei/zuoye.py
@@ -11,10 +11,11 @@ with open(op, "r") as f:
     iplist = [x.split(" ")[0] for x in f ] #  取IP
     a = ( (i,iplist.count(i)) for i in {x for x in iplist} ) #去重并统计重复数量
 
-    c = sorted(a, key=lambda x:x[1], reverse=True)[:10] #排序，拿前10个
+    c = sorted(a, key=lambda x:x[1], reverse=True)[:10] #排序取前10个
     for i in c:
-        print i[0],i[1]
+        print "%s,%10s" % (i[0],i[1])
 
 
-
+print 
+print "time:"
 print time.time() - start
