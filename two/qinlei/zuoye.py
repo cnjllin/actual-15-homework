791d72d5e597533c3b43d472ae2a406ea07d943f
greatestray <yutian163@126.com>
2017-07-31 16:41:38 -0400

    first commit

---
+++ two/qinlei/zuoye.py
@@ -0,0 +1,20 @@
+#/usr/bin/env python
+
+#coding:utf-8
+
+import time
+
+start = time.time()
+op = "/root/access.txt"
+with open(op, "r") as f:
+
+    iplist = [x.split(" ")[0] for x in f ] #  取IP
+    a = ( (i,iplist.count(i)) for i in {x for x in iplist} ) #去重并统计重复数量
+
+    c = sorted(a, key=lambda x:x[1], reverse=True)[:10] #排序，拿前10个
+    for i in c:
+        print i[0],i[1]
+
+
+
+print time.time() - start
