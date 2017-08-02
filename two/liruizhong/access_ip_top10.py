14d6ec5853620f62f70635c6a94f0cbe27f5528a
liruizhong <1126785053@qq.com>
2017-08-02 22:47:27 +0800

    update access_top10 homework

---
+++ two/liruizhong/access_ip_top10.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+#-*- coding:utf-8 -*-
+
+'''
+作业：    打印出nginx访问日志中，访问次数前十的IP以及对应的访问次数
+思路分析：1、打开log文件，并按行读取
+          2、对读取的每一行进行分割，切片，获取到访问IP
+          3、将获取的IP作为字典的key,存入到字典中
+          4、出现重复的IP，则将此IP的value进行加1操作
+          5、将这样的字典key,value 以value进行排序
+          6、取出后十对，即是访问次数前十的IP
+'''
+
+import time
+
+d = {}
+start = time.clock()
+# 打开并读取日志文件
+with open('access.txt','r') as f:
+    for i in f:
+        # 获取每行的访问IP
+        ip = i.split(' ')[0]
+        # 进行判断，如果访问IP没有在字典的key中，则对字典进行添加
+        if ip not in d.keys():
+            d.update({ip:1})
+        # 如果已经在字典的key中，则对相应的key对应的value进行加1
+        else:
+            count = d[ip] + 1
+            d[ip] = count
+    # 对字典以value 进行排序，生成一个列表
+    list1 = sorted(d.items(),key=lambda item:item[1])
+    # 遍历列表，取出访问次数前十的IP及次数
+    for i in range(1,11):
+        access_ip = list1[-i][0]
+        access_count = list1[-i][1]
+        print "\033[32m%-15s\033[0m was visited \033[33m%s\033[0m times" % (access_ip,access_count)
+
+end = time.clock()
+# 打印出程序运行的时间
+print("The function run time is : \033[32m%.03f\033[0m seconds" %(end-start))
---
+++ two/liruizhong/results.md
@@ -0,0 +1,16 @@
+### 作业演示
+
+```
+[root@LearnPython liruizhong]# python access_ip_top10.py
+123.174.51.164  was visited 6958 times
+111.85.34.165   was visited 2307 times
+118.112.143.148 was visited 1617 times
+117.63.146.40   was visited 1489 times
+118.182.116.39  was visited 1404 times
+1.48.219.30     was visited 1352 times
+60.222.231.46   was visited 1132 times
+10.35.1.82      was visited 1129 times
+27.227.163.200  was visited 943 times
+58.253.6.133    was visited 880 times
+The function run time is : 0.890 seconds
+```
