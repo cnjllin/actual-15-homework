b74ee0a759882d5f1c1abac131db69e9ee322449
superlk <651002081@qq.com>
2017-08-02 17:20:14 +0800

    Update ip_top10.py

--- two/liukai/ip_top10.py
+++ two/liukai/ip_top10.py
@@ -3,6 +3,7 @@
 # date 2017.8.2
 # mail:651002081@qq.com
 # 取日志文件前10的ip，和对应次数
+
 # 导入模块
 import time
 # 开始时间
@@ -11,6 +12,7 @@ a=time.time()
 my_dict={}
 # 定义一个空列表
 my_list_sort=[]
+
 # 打开文件
 with open('access.txt') as f:
     # 遍历文件每一行
@@ -21,16 +23,19 @@ with open('access.txt') as f:
         tup=tem[0]
         # 把列表里的ip当做key，出现次数为value放入到空字典里
         my_dict[tup]=my_dict.get(tup,0)+1
+
 # 对字典冒泡排序
 my_list_sort=my_dict.items()
 for i in range(len(my_list_sort)-1):
     for j in range(len(my_list_sort)-1-i):
         if my_list_sort[j][1]>my_list_sort[j+1][1]:
             my_list_sort[j],my_list_sort[j+1]=my_list_sort[j+1],my_list_sort[j]
+
 # 打印输出
 for i in range(10):
     tmm=my_list_sort.pop()
     print '访问次数第%s的ip:%s,访问次数:%s'%((i+1),tmm[0],tmm[1])
+
 # 结束时间
 b=time.time()
 print'使用时间:%s'%(b-a)
