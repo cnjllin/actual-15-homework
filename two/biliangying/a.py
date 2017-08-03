4bec6488e98c5a8b358dc442fc1929bc8d2380cb
bilingying <1406718771@qq.com>
2017-08-03 16:59:02 +0800

    作业

---
+++ two/biliangying/a.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+#coding:utf-8
+#defaultdict会在初始化时指定默认值.
+from collections import defaultdict
+fo = open('access.txt')
+#读取IP
+a = []
+for i in fo.readlines():
+	a.append(i.split(' ')[0])
+#给default_dict传入int，则可以用来计数：
+b = defaultdict(int)
+for j in a:
+	b[j] += 1
+#字典按value排序
+b=sorted(b.items(),key=lambda item:item[1],reverse=True)[0:10]
+for x in b:
+        #把元祖转换为列表
+	y = (list(x))
+	print "ip:%s，出现的次数：%s" % (y[0],y[1])
