45ef55daf090dff3f56c426df7f0ae8b50d0c5da
zxlccc <2193812481@qq.com>
2017-08-05 01:34:36 +0800

    homework

--- two/zengxianli/homework.py
+++ two/zengxianli/homework.py
@@ -2,21 +2,22 @@
 #coding:utf-8
 
 #打开日志文件，以列表的形式储存，每一行为一个字符串为列表中的一个元素，列表名为data
-with open('access.log') as f:
+with open('access.txt') as f:
        data = f.readlines()
 
-data1=[] #新建列表，用于存放IP地址
+##data1=[] #新建列表，用于存放IP地址
 data2=[] #新建列表，用于存放IP地址
 tmp={}   #新建字典用于存放IP地址及出现次数，字典的格式为tmp{IP:次数}
 
 
 for i,ip in enumerate(data):
-        data1.append(ip.split(" ")[-3].split(":")[0]) #将列表data中的每个元素（字符串）转换成列表，并提取新列表中的倒数第三个元素（IP地址），存入data2中
+        ##data1.append(ip.split(" ")[-3].split(":")[0]) 
+
         data[i]=ip.split(" ")[0] #将列表data中的每个元素（字符串）转换成列表，并提取新列表中的第一个元素（IP地址），更换掉原data的元素
 
 
-data1.remove(data1[-1])
-data=data+data1 #将data和data2合并成新的列表，并重命名为data，即新列表data中每一个元素为日志中出现的每一个IP地址
+##data1.remove(data1[-1])
+##data=data+data1 #将data和data2合并成新的列表，并重命名为data，即新列表data中每一个元素为日志中出现的每一个IP地址
 data2=data      #将新列表data复制一份，并将副本命名为data2
 data=list(set(data)) #将data2去重
 
