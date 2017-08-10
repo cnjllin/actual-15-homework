5cc0a412bbffcffe6528fb515d9e5c088d6245bc
liuyang <liu1220xia@sina.com>
2017-08-10 11:07:11 +0800

    commit by liuyang

--- three/liuyang/registor_login_function.py
+++ three/liuyang/registor_login_function.py
@@ -3,52 +3,62 @@
 #注册登录函数
 
 #注册系统
-def registor(name,pwd,re_pwd):
+def registor():
 	with open('user_passwd.txt','a+') as f :
-	#判断密码是否为空
-			if name == '':
-				print '用户名不能为空，请重新输入！'
-			else:
-			 	if pwd == re_pwd:
-					print '用户%s注册成功！'%(name)
-					f.write('%s:%s\n'%(name,pwd))
+		count = 0
+		while count <3:
+			user_name = raw_input('请输入用户名：')
+			if user_name == ' ':
+			    print '用户名不能为空，请重新输入！'
+		 	else:
+				user_pwd = raw_input('输入密码： ')
+				re_pwd = raw_input('请确认密码： ')
+				if user_pwd == re_pwd:
+					print '%s用户注册成功！'%(user_name)
+					f.write('%s:%s\n'%(user_name,user_pwd))
+					break
 				else:
-					print '密码不匹配，请重新输入！'
+					print '两次密码不匹配，请重新输入！'
+				count += 1
+		else:
+			print '输入错误超过三次'
 
 
 #登录系统
-def login(name,pwd):
+def login():
 	temp_dict = {}
 	with open('user_passwd.txt','a+') as f:
 		for line in f:
 			user_name = line.strip('\n').split(':')[0]
 			user_pwd = line.strip('\n').split(':')[1]
 			temp_dict[user_name]=user_pwd
-			#print temp_dict
-		#登录判断用户名是否存在：
-		if name in temp_dict:
-			#print temp_dict[name]
-			#输入密码
-			if pwd == temp_dict[name]:
-				print '用户%s登录成功'%(name)
+		#print user_passwd_dict
+		#登录
+		count = 0
+		while count <3:
+			name = raw_input('输入用户名： ')
+			if name in temp_dict:
+				pwd = raw_input('输入密码： ')
+				if pwd == temp_dict[name]:
+					print '登录成功'
+					break
+				else:
+					print '密码输入错误'
+				
 			else:
-				print '密码输入错误'
+				print '用户不存在，请确认！'	
+			count += 1
 		else:
-			print '用户不存在，请确认！'
+			print '输入错误超过3次'
 
 #登录或者注册
 def choose():
     action =raw_input('input a action:registor/login ').strip()
 
     if action == 'registor':
-        name = raw_input('输入用户名: ').strip()
-        pwd = raw_input('输入密码: ').strip()
-        re_pwd =raw_input('再次输入密码: ').strip()
-        act = registor(name,pwd,re_pwd)
-    elif action =='login':
-    	name = raw_input('输入用户名: ').strip()
-        pwd = raw_input('输入密码: ').strip()
-        act= login(name,pwd)
+        act = registor()
+    elif action == 'login':
+        act = login()
     else:
     	print '选择输入错误'
     return act
