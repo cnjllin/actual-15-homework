60ab525b5ac802703d969c499957dcac4aac7e3e
root <lqingming@163.com>
2017-08-11 00:17:59 +0800

    ngnix

---
+++ three/liuqingming/ngnix.py
@@ -0,0 +1,16 @@
+#!/bin/env python
+# -*- encoding:utf-8 -*-
+
+#import day2_homewk4
+#print day2_homewk4.ip_tops
+
+from day2_homewk4 import ip_tops #导入模块中的列表
+print ip_tops
+
+with open('IP_top10.html','w') as f:
+    f.write('<html>\n\n<title>\nNgnix日志IP前10\n</title>\n<h><style="color:red"> IP </h>\n<body>\n\n<table border=1 >\n<table style border=1 >\n')
+    f.write('<tr><th>IP</th><th>数量</th></tr>')
+    for i in ip_tops:
+        f.write('<tr><td>%s</td><td>%d</td></tr>\n' % (i[0],i[1]))
+    f.write('</table>\n</body>\n</html>\n')
+print ""
