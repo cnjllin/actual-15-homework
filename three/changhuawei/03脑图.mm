78ee7994cf99dfd422f1652e716a1f9c4318c690
root <root@kdc4.58os.org>
2017-08-12 22:55:11 +0800

    update

---
+++ three/changhuawei/03脑图.mm
@@ -0,0 +1,20 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node CREATED="1502548771278" ID="ID_1633809788" MODIFIED="1502548775246" TEXT="03">
+<node CREATED="1502549231941" ID="ID_1816311759" MODIFIED="1502549240501" POSITION="right" TEXT="&#x51fd;&#x6570;">
+<node CREATED="1502549388839" ID="ID_743034012" MODIFIED="1502549456467" TEXT="def  add(x):"/>
+<node CREATED="1502549428538" ID="ID_824651078" MODIFIED="1502549502212" TEXT="print  x"/>
+<node CREATED="1502549474933" ID="ID_1362674828" MODIFIED="1502549482686" TEXT="return x"/>
+</node>
+<node CREATED="1502549266067" ID="ID_1499046361" MODIFIED="1502549278613" POSITION="right" TEXT="&#x6570;&#x636e;&#x7c7b;&#x578b;">
+<node CREATED="1502549514141" ID="ID_1250210278" MODIFIED="1502549534944" TEXT=".join()&#x51fd;&#x6570;&#x7528;&#x6cd5;&#xff1a;&#x201c;&#x5206;&#x9694;&#x7b26;&#x201d;.join(&#x5217;&#x8868;)&#x2014;&#x628a;&#x5217;&#x8868;&#x8f6c;&#x6362;&#x6210;&#x5b57;&#x7b26;&#x4e32;"/>
+<node CREATED="1502549543800" ID="ID_271944805" MODIFIED="1502549545102" TEXT="In [10]: www = [&quot;a&quot;,&quot;b&quot;,&quot;c&quot;]"/>
+<node CREATED="1502549545791" ID="ID_1138239905" MODIFIED="1502549554665" TEXT="In [11]: print &apos;-&apos;.join(www)"/>
+<node CREATED="1502549567033" ID="ID_1413244943" MODIFIED="1502549569402" TEXT="split()&#x51fd;&#x6570;&#x7528;&#x6cd5;&#xff1a;&#x5b57;&#x7b26;&#x4e32;.split(&#x2018;&#x5206;&#x9694;&#x7b26;&#x2019;)&#x2014;&#x628a;&#x5b57;&#x7b26;&#x4e32;&#x53d8;&#x6210;&#x5217;&#x8868;"/>
+<node CREATED="1502549570277" ID="ID_308258527" MODIFIED="1502549577837" TEXT="In [25]: aa = &apos;www.baidu.com&apos;"/>
+</node>
+<node CREATED="1502549380697" ID="ID_103018120" MODIFIED="1502549384853" POSITION="right" TEXT="html">
+<node CREATED="1502549593811" ID="ID_349257510" MODIFIED="1502549610286" TEXT="&lt;table style=&quot;height: 100px;&quot; border=&quot;5&quot; cellspacing=&quot;0&quot; bordercolor=&quot;&#x9;#000000&quot; cellpadding=&quot;8&quot;&gt; &#x9;&lt;tr&gt; &#x9;&lt;td&gt;&#x6570;&#x91cf;&lt;/td&gt; &#x9;&lt;td&gt;IP&lt;/td&gt; &#x9;&lt;/tr&gt; &#x9;&lt;tr&gt; &#x9;&lt;td&gt;100&lt;/td&gt; &#x9;&lt;td&gt;192.168.1.1&lt;/td&gt; &#x9;&lt;/tr&gt; &#x9;&lt;tr&gt; &#x9;&lt;td&gt;200&lt;/td&gt; &#x9;&lt;td&gt;192.168.1.2&lt;/td&gt; &#x9;&lt;/tr&gt; &lt;/table&gt; "/>
+</node>
+</node>
+</map>
