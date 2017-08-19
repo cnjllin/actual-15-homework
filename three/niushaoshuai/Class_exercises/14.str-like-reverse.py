6c4ddb43fec27da3a829b46d46e95567cd7616f8
zhangbaocheng@ship56.net <zhangbaocheng@ship56.net>
2017-08-19 09:05:25 +0800

    1

--- three/niushaoshuai/Class_exercises/14.str-like-reverse.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-# --*-- coding=UTF-8 --*--
-my_str="reboot"
-my_list=[]
-for n in my_str:
-    my_list.insert(0,n)
-    
-my_str1="".join(my_list)
-print "{} after reverse is {}".format(my_str,my_str1)
