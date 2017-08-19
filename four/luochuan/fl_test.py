dadec6909f0e5697164fb7d1d2a198e2739cc85c
dda163103 <dda1013@163.com>
2017-08-19 22:49:52 +0800

    luochuan homework

--- four/luochuan/fl_test.py
+++ four/luochuan/fl_test.py
@@ -1,5 +1,5 @@
-# -*- coding: utf-8 -*-
-from flask import Flask,request,render_template
+#coding:utf-8
+from flask import Flask,request,render_template,redirect
 
 app = Flask(__name__)
 
@@ -13,35 +13,44 @@ def register():
     if request.method == 'GET':
         return render_template('register.html')
     if request.method == 'POST':
-        user = request.form.get('user')
-        pwd = request.form.get('pwd')
-        if len(user) == 0 or len(pwd) == 0:
-            return "用户名或者密码为空"
-    with open('userinfo.txt','a') as f:
-        f.write(user+':'+pwd+'\n')
+        user01 = request.form.get('user','')
+        pwd = request.form.get('pwd','')
+        if len(user01) == 0 or len(pwd) == 0:
+            return "username or password is null"
+    with open('E:\\fl_test\\zhuce.log','a+') as f1:
+        f1.write(user01+':'+pwd+'\n')
     return redirect('/userlist')
+
+@app.route('/userlist')
+def userlist():
+    userlist = []
+    with open('E:\\fl_test\\zhuce.log') as f2:
+        for l in f2:
+            userlist.append(l.split(':'))
+        return render_template('userlist.html',userxxx=userlist)
+
 
 
 @app.route('/login/', methods=['GET'])
 def submit():
      return render_template('submit.html')
 
-@app.route('/login/', methods=['POST'])
+@app.route('/login/submit', methods=['POST'])
 def login():
     with open('E:\\fl_test\\zhuce.log', 'r')as f:
         user = request.form.get('username','')
         passwd = request.form.get('password','')
-        dict = {}
+        x = {}
         Username = []
 
         for line in f.readlines():
-            dict[line.strip().split(":")[0]] = line.strip().split(":")[1]
+            x[line.strip().split(":")[0]] = line.strip().split(":")[1]
 
-        for i in dict:
+        for i in x:
             Username.append(i)
 
         if user in Username:
-            if passwd == dict.get(user):
+            if passwd == x.get(user):
                 choice = '1'
             else:
                 choice = '0'
