b5f279b6e1c3cc803c869f2d3fa3a7da7ccef123
撼地神杜 <handishendu@qq.com>
2017-08-19 20:36:40 +0800

    update four

--- four/dujiayang/homework/app.py
+++ four/dujiayang/homework/app.py
@@ -14,14 +14,15 @@ def index_1():
 
 
 
-@app.route('/register',methods=['POST','GET'])
+@app.route('/zhuce',methods=['POST','GET'])
 def reg():
-	return render_template('signin.html')
-	mtd = request.form if request.method == 'POST' else request.args
-	name = mtd.get('username')
-	password = mtd.get('passwd')
+	#mtd = request.form if request.method == 'POST' else request.args
+	#name = mtd.get('username')
+	#password = mtd.get('passwd')
+	name = request.form.get('username')
+	password = request.form.get('passwd')
 	print name,password
-	register(name,password)
+	invoke.register(name,password)
 
 
 @app.route('/denglu',methods=['POST','GET'])
@@ -30,7 +31,6 @@ def login():
         mtd = request.form if request.method == 'POST' else request.args
         name = mtd.get('username')
         password = mtd.get('passwd')
-	print name,password
         login(name,password)
 
 if __name__== '__main__':
