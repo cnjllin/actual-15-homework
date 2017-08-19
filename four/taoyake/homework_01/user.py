8481b31ec9b780cade5a3249edc60047867a32c1
陶亚可 <taoyake@playcrab.com>
2017-08-19 16:36:01 +0800

    update

---
+++ four/taoyake/homework_01/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def GetUser():
+    user_list = []
+    f = open("userinfo.txt", 'r')
+    for user in f:
+        if not user:
+            break
+        line = user.strip('\n').split(':')
+        user_list.append(line)
+    return user_list
+
+# 用户登录验证函数
+def UserLogin(username, password):
+    all_user = GetUser()
+    for user in all_user:
+        print user[0],user[1]
+        if username == user[0] and password == user[1]:
+            return True
+    return False
