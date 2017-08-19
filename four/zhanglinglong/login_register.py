fc1026d56ae181c3f82299fbdb3fd2ae01cc5a4a
linglong0820 <zhanglinglong@163.com>
2017-08-19 14:54:20 +0800

    修改脚本注释

--- four/zhanglinglong/login_register.py
+++ four/zhanglinglong/login_register.py
@@ -9,10 +9,12 @@ app = Flask(__name__)
 
 @app.route('/login/',methods=["GET","POST"])
 def login():
+    #如果前端获取的方法是POST，则接受数据
     if request.method=='POST':
         username = request.form.get('username')
         password = request.form.get('password')
         print username,password
+        #打开用户文件，将用户名存入一个列表，用户名和密码存入一个字典
         f = open('user.txt')
         l = []
         k = {}
@@ -22,10 +24,12 @@ def login():
             b = i.split(':')[1]
             k[a] = b
             l.append(a)
+        #判断用户名是否存在
         if username not in l:
             return "用户名不存在"
             exit()
         else:
+            #判断密码是否正确
             if password == k.get(username):
                 return "欢迎%s登录" %username
             else:
@@ -36,15 +40,18 @@ def login():
 
 @app.route('/register/',methods=["GET","POST"])
 def register():
+    #如果前端获取的方法是POST，则接受数据
     if request.method=='POST':
         username = request.form.get('username')
         new_password = request.form.get('new_password')
         confirm_password = request.form.get('confirm_password')
         print username,new_password,confirm_password
         m = open('user.txt','a+')
+        #判断密码是否为空
         if len(username) == 0:
             return "用户名不能为空"
             exit()
+        #打开用户文件，将用户名存入一个列表，检查用户名是否被注册
         l = []
         for i in m.readlines():
             a = i.split(':')[0]
@@ -52,8 +59,10 @@ def register():
         if username in l:
             return "该账户已被注册"
             exit()
+        #判断密码是否为空
         if len(new_password) == 0:
             return "密码不能为空"
+        #判断两次输入密码是否相同
         if confirm_password == new_password:
             m.write('%s:%s\n' %(username,new_password))
             return "注册成功，您的账号为:%s,密码为:%s,请妥善保管" %(username,new_password)
