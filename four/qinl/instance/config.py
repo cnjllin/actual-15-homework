1f98687438dbe18bcb657d06f129ea10f19d8db3
greatestray <yutian163@126.com>
2017-08-20 06:11:27 -0400

    first commit

---
+++ four/qinl/instance/config.py
@@ -0,0 +1,9 @@
+LOG = r"C:\Users\Administrator\flaskr\ccess.txt"
+
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+DEBUG = True
+
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:yutian@192.168.78.128/flaskr'
+SQLALCHEMY_TRACK_MODIFICATIONS = True
