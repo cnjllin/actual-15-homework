319235908791a5e23e9fab8d1c8fa1dbd28947d5
niushaoshuai <296636419@qq.com>
2017-08-21 22:12:32 +0800

    update

--- four/niushaoshuai/home_task/userinfo.py
+++ four/niushaoshuai/home_task/userinfo.py
@@ -1,29 +1,37 @@
 #!/usr/bin/python
 # --*-- coding:UTF-8 --*--
+import userconf
 
-f = open('user_message.db', 'r')
-message = f.readlines()
-user=dict()
-for  line in message:
-    mu = line.split(':')[0]
-    mp = line.split(':')[1].strip(' \n')
-    user[mu]=mp
-f.close()
+def GetUser():
+    f = open(userconf.USER_FILE, 'r')
+    message = f.readlines()
+    f.close()
+    user_info=dict()
+    for  line in message:
+        mu = line.strip(' \n').split(':')[0]
+        mp = line.strip(' \n').split(':')[1]
+        user_info[mu]=mp
+    return user_info
 
 def checkout_user_pass(u, p):
+    user=GetUser()
     if u in user.keys():
-        if user[u] == p:
+        if user[u] == p :
             return True
+
         else:
             return False
+   
     else:
-        return False
+            return False
 
 def checkout_user_exist(u):
+    user=GetUser()
     if  u in user.keys():
         return True
     else:
         return False
 if __name__ == '__main__':
-    print checkout_user_pass('niushaoshuai1','123456')
-    print checkout_user_exist('niushaoshuai12')
+#    print checkout_user_pass('niushaoshuai1','123456')
+#    print checkout_user_exist('niushaoshuai12')
+    print checkout_user_pass('owen8','123')
