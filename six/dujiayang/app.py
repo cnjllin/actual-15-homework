fc2a5afe65b60aca9329b646f5b3edec2da2a15c
撼地神杜 <handishendu@qq.com>
2017-09-03 12:00:04 +0800

    hehe

---
+++ six/dujiayang/app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+#coding:utf8
+
+from flask import Flask,request,redirect,render_template
+
+app= Flask(__name__)
+
+@app.route('/')
+@app.route('/index/')
+def index():
+	username = "wd"
+	render_template("index.html",username=usernaem)
+
+@app.route('/reg/',methods=['POST','GET'])
+def reg():
+	if request.method == "POST":
+		data = {k:v[0] for k,v in dict(request.form).iteritems()}	
+		print data
+		field = ["username","passwd","role"]
+		result = insert('dudjiayang',field,data)
+		if result[['code'] == 0:
+			return redirect('/login/')
+		else:
+			return render_template("register.html",result=result)
+	return render_template("register.html")
+
+
+if __name__ == '__main__':
+	app.run(host='0.0.0.0',debug=True)
---
+++ six/dujiayang/utils.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+#_*_ codind: utf-8 _*_
+
+import MySQLdb as mysql
+
+#db=mysql.connect('127.0.0.1','root','1234','reboot15',charset='utf8')
+db=mysql.connect('121.43.191.76','reroot','reboot','reboot15',charset='utf8')
+db.autocommint(True)
+cur = db.cursor()
+
+def insert(table,field,data):
+	sql = "insert into %s(%s) values(%s)" % (table,','.join(field),','.join(['"%s"' % data[i] for i in field]))
+	res = cur.execute(sql)
+	if res:
+		result = {"code":0,"msg":"insert ok"}
+	else:
+		result = {"code":1,"msg":"insert fail"}
+	return result
+	
