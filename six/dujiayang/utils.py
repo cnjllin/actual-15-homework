73bcade35f40ea6f5e9b0b981e5c875990e2a868
撼地神杜 <handishendu@qq.com>
2017-09-03 14:58:56 +0800

    up

--- six/dujiayang/utils.py
+++ six/dujiayang/utils.py
@@ -8,12 +8,47 @@ db=mysql.connect('121.43.191.76','reroot','reboot','reboot15',charset='utf8')
 db.autocommint(True)
 cur = db.cursor()
 
+#注册
 def insert(table,field,data):
 	sql = "insert into %s(%s) values(%s)" % (table,','.join(field),','.join(['"%s"' % data[i] for i in field]))
+	print sql
+	try:
 	res = cur.execute(sql)
-	if res:
 		result = {"code":0,"msg":"insert ok"}
-	else:
+	except :
 		result = {"code":1,"msg":"insert fail"}
 	return result
+	
+#登录
+def getone(table,field,data):
+	if data.has_key("username"):
+		sql = 'select * from %s where username = "%s"' % (table,data["username"]) 
+	else:
+		sql = 'select * from %s where id = "%s"' % (table,data["id"])
+	print sql
+	cur.execute(sql)
+	res = cur.fetchone()
+	if res:
+		user = {k:res[i] for i,k in enumerate(field)}
+		print user
+		result = {"code":0,"msg":user}
+	else:
+		result = {'code':1,'msg':'data is null'}
+	return result
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 	
