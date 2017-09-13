11b36db1b8c662e8847f0f6a160930e1fdb4e07f
dujiayang <handishendu@qq.com>
2017-09-13 18:14:43 +0800

    hehe

--- seven/dujiayang/today/app.py
+++ seven/dujiayang/today/app.py
@@ -1,35 +1,134 @@
 #!/usr/bin/env python
-#coding:utf-8
-from flask import Flask,render_template,request
+#-*- coding:utf-8 -*-
+
+from flask import Flask,request,render_template,redirect,session
+from utils import insert,getone,listall,updateuser,delete
 import json
 
 app = Flask(__name__)
+app.secret_key="abcdefhehehaha"
 
+field = ['id','username','password','role']
+
+# 首页
 @app.route('/')
-def userlist():
-    return render_template("userlist.html")
+@app.route('/index/')
+def index():
+	#if not session:
+	#	return redirect('/login/')
+	#return redirect('/userinfo/')
+	return render_template('index.html')
+# 注册
+@app.route('/reg/',methods=['GET','POST'])
+def reg():
+	if request.method == 'POST':
+		data = {k:v[0] for k,v in dict(request.form).items()}
+		field = ['username','password','role']
+		result = insert('user1',field,data)
+		if result['code'] == 0:
+			return redirect('/login/')
+		else:
+			return render_template('register.html',result=result)
+	return render_template('register.html')
 
-@app.route('/login/',methods=['POST','GET'])
+# 登录
+@app.route('/login/',methods=['GET','POST'])
 def login():
 	if request.method == 'POST':
 		data = {k:v[0] for k,v in dict(request.form).iteritems()}
-		result = {'code':0,'msg':data}
-		#print result
-		return json.dumps(result)
-    	return render_template("login.html")
+		result = getone('user1',field,data)
+		if result['code'] == 0:
+			if result['msg']['password'] == data['password']:
+				session['username'] = data['username']
+				session['role'] = result['msg']['role']
+				print "session"
+				print session
+				if session['role'] == 0:
+					return redirect('/userlist/')
+				else:
+					return redirect('/userinfo/')
+			else:
+				result = 'code0 password is fail'
+				print result
+				return render_template('login.html',result=result)	
+		else:
+			result = 'code1 user is not exist'
+			print result
+			return render_template('login.html',result=result)
+	return render_template('login.html')
 
+# common用户信息
+@app.route('/userinfo/')
+def userinfo():
+	if not session:
+		return redirect('/login/')
+	data = {'username':session['username']}	
+	result = getone('user1',field,data)
+	if result['code'] == 0:
+		result = result['msg']
+		return render_template('userinfo.html',result=result)
+	return render_template('userinfo.html',result=result)
+	
 
-@app.route('/getone/')
-def getone():
-	username = request.args.get("username","")
-	user = {'name':username,'passwd':'123456'}
-	return json.dumps(user)
+# 用户列表,admin才能看
+@app.route('/userlist/')
+def userlist():
+	if not session or session['role'] == 1:
+		return redirect('/login/')
+	result = listall('user1',field)
+	print "app_userlist_result"
+	print result
+	ret = result['msg']
+	print ret
+	if result['code'] == 0:
+		return render_template('userlist.html',result=result['msg'])
+	return render_template('userlist.html',result=result)
 
-#@app.route('/')
-#def hello_world():
-#    name="liuziping"
-#    return render_template("userlist.html",user_name=name)
+# 更新
+@app.route('/update/',methods=['GET','POST'])
+def update():
+	if not session:
+		return redirect('/login/')
+	if request.method == 'GET':
+		data = {}
+		data['id'] = int(request.args.get('id'))
+		print data
+		result = getone('user1',field,data) 
+		print result
+		if result['code'] == 0:
+			return render_template('update.html',result=result['msg']) 	
+	if request.method == 'POST':
+		data = {k:v[0].strip() for k,v in dict(request.form).iteritems()}
+		result = updateuser('user1',field,data)
+		print "post result"
+		print result
+		if result['code'] == 0 and session['role'] == 0:
+			return redirect('/userlist/')
+		elif result['code'] == 0 and session['role'] == 1:
+			return redirect('/userinfo/')
+		else:
+			return render_template('update.html',result=result)
 
-if __name__=='__main__':
-    app.debug=True
-    app.run(host='0.0.0.0',port=9999)
+# 删除用户
+@app.route('/delete/')
+def delete_user():
+	if not session:
+		return redirect('/login/')
+	uid = int(request.args.get('id'))
+	result = delete('user1',uid)
+	if result['code'] == 0 and session['role'] == 0:
+		return redirect('/userlist/')
+	elif result['code'] == 0 and session['role'] == 1:
+		return redirect('/userinfo/')
+	else:
+		result = "user is nulled"
+		return redirect('/userinfo/',result=result)
+
+# 销毁session
+@app.route('/logout/')
+def logout():
+	session.clear()
+	return redirect('/index/')
+
+if __name__ == '__main__':
+	app.run(host='59.110.12.72',port=9999,debug=True)
---
+++ seven/dujiayang/today/log/test.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# coding=utf-8
+f = open('test.log')
+res = {}
+for l in f:
+    arr = l.split(' ')
+    # 获取ip url 和status
+    ip = arr[0]
+    url = arr[6]
+    status = arr[8]
+    # ip url 和status当key，每次统计+1
+    res[(ip,url,status)] = res.get((ip,url,status),0)+1
+print "\n"
+print "\n"
+# 生成一个临时的list
+res_list = [(k[0],k[1],k[2],v) for k,v in res.items()]
+print "\n"
+print "\n"
+# 按照统计数量排序，打印前10
+for k in sorted(res_list,key=lambda x:x[3],reverse=True)[:2]:
+    print k
