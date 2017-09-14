6a0aa421e2ed9c9a957f4140689d7843f7cdaef6
root <root@py.(none)>
2017-09-14 18:02:41 +0800

    zuoye

---
+++ eight/hanchaoguo/app.py
@@ -0,0 +1,150 @@
+#!/usr/bin/python
+#coding:utf-8
+
+from flask import Flask,render_template,request,redirect,session
+from utils import insert,getone,select,update,delete
+import json
+app=Flask(__name__)
+app.secret_key = 'askdmsakldalsdk'
+filed = ['id','username','password','role']
+
+
+# 首页
+@app.route('/',methods=['GET','POST'])
+def sigin():
+    if not session:
+        return redirect('/login')
+    username = "hanchaoguo"
+    return render_template('index.html',username=username)
+
+
+# 用户列表
+@app.route('/userlist/')
+def userlist():
+    if not session:
+        return redirect('/login/')
+    res = select('user',filed)
+    return  render_template('userlist.html',res=session,result=res['msg'])
+
+# 注册
+@app.route('/reg/',methods=['GET','POST'])
+def reg():
+    if request.method == 'POST':
+        users = {k:v[0] for k,v in dict(request.form).items()}
+        filed=['username','password','role']
+        res =  insert('user',filed,users)
+        if res['code'] == 0:
+            return render_template ("reg.html",msg=res['msg'])
+        else:
+            return  render_template ("reg.html",mag=res['msg'])
+    return render_template ("reg.html",msg=" ")
+
+
+# 登录
+@app.route('/login/',methods=['GET','POST'])
+def login():
+   if request.method == 'POST':   
+      dat = {k:v[0] for k,v in dict(request.form).items()}
+      res = getone("user",filed,dat)
+      if res['code'] == 0  and  res['msg']['password'] == dat['password']:
+            session['username'] = dat['username']
+            session['role'] = res['msg']['role']
+            return json.dumps(res)
+      else:
+            return json.dumps(res)
+   return  render_template('login.html',msg=" ") 
+
+# 用户修改个人信息入口
+@app.route('/userinfo1/')
+def userinfo():
+    if not session:
+        return redirect('/login/')
+    uid = request.args.get('id')
+    data={'id':uid}
+    res = getone("user",filed,data)
+    data=res['msg']
+    print data
+    return json.dumps(data)
+       
+
+# 用户界面
+@app.route('/userinfo/')
+def usifo():
+    if not session:
+       return redirect('/login/')
+    username = session['username']
+    user_dict={'username':username}
+    result=getone('user',filed,user_dict)
+    return render_template('userinfo.html',res=session,result=result['msg'])
+
+
+# 用户更新
+@app.route('/updata/',methods=['GET','POST'])
+def up():
+    if not session:
+        return redirect('/logout/')
+    if request.method == 'POST': 
+        dat = {k:v[0] for k,v in dict(request.form).items()}
+        print dat
+        data = update("user",filed,dat)
+        return json.dumps(data)
+        # return redirect('/login/')
+    dat = {k:v[0] for k,v in dict(request.args).items()}
+    print dat
+    data = update("user",filed,dat)
+    return json.dumps(data)
+
+# 修改个人信息
+@app.route('/upda/',methods=['GET','POST'])
+def upda():
+    if not session:
+        return redirect('/logout/')
+    if request.method == 'POST':
+        filed = ['id','username','password']
+        dat = {k:v[0] for k,v in dict(request.form).items()}
+        print dat
+        data = update("user",filed,dat)
+        return json.dumps(data)
+    dat = {k:v[0] for k,v in dict(request.args).items()}
+    print dat
+    data = update("user",filed,dat)
+    return json.dumps(data)
+
+
+# 修改密码
+@app.route('/user/chpwdoneself',methods = ['GET','POST'])
+def uppasswd():
+    if not session:
+        return redirect('/login/')
+    dat={k:v[0] for k,v in dict(request.form).items()}
+    res = getone("user",filed,dat)
+    if res['msg']['password'] == dat['oldpasswd']:
+        dat['password']=dat['newpasswd']
+        file = ['id','password']
+        data = update("user",file,dat)    
+        return json.dumps(data)
+    data = {'errmsg':"error"}
+    return json.dumps(data)    
+
+
+# 删除
+@app.route('/delete/',methods = ['GET','POST'])
+def delne():
+    if not session:
+        return redirect('/login/')
+    data={k:v[0] for k,v in dict(request.args).items()}
+    res = delete("user",filed,data)
+    print data
+    return json.dumps(res)
+    return redirect('/login/')
+
+# 注销
+@app.route('/logout/')
+def logout():
+    if session:
+        session.clear()
+    return redirect('/login/')
+    
+
+if __name__=='__main__':
+    app.run(host='0.0.0.0',port=500,debug=True)
---
+++ eight/hanchaoguo/utils.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+#coding:utf-8
+import MySQLdb as mysql
+db=mysql.connect(host="127.0.0.1",user="root",passwd="123456",db="reboot15",port
+=3306,charset='utf8')
+db.autocommit(True)
+cur = db.cursor()
+
+
+
+
+#增加
+def insert(table,filed,users):
+    sql = "insert into %s (%s) values (%s);" %(table,','.join(filed), ','.join([ '"%s"' %users[n] for n in filed]))
+    print sql
+    res = cur.execute(sql)
+    if res:
+        result ={'code':0,'msg':'insert is ok'}
+    else:
+        result ={'code':1,'msg':'insert is error'}
+    return result
+
+# 登录
+def getone(table,filed,data):
+    if data.has_key("username"):
+        sql = 'select * from %s where username= "%s"' %(table,data['username'])
+    else:
+        sql = 'select * from %s where id = "%s"' %(table,data['id'])
+    cur.execute(sql)
+    res = cur.fetchone()
+    if res:
+      users = {k:res[i] for i,k in enumerate(filed)}
+      user = {'code':0,'msg':users}
+    else:
+      user= {'code':1,'errmsg':'no users'}
+    return user
+    
+
+#查询
+def select(table,field):
+    sql = "select * from %s" % table
+    cur.execute(sql)
+    res = cur.fetchall()
+    if res:
+       user = [{k:row[i] for i,k in enumerate(field)} for row in res]
+       result = {'code':0,'msg':user}
+    else:
+       result = {'code':1,'msg':'data is null'}
+    return result
+# 更新
+def update(table,filed,data):
+    result=['%s = "%s"' %(k,data[k]) for k in filed]
+    sql = "update %s set %s where id = %s" %(table,','.join(result),data['id'])
+    print sql
+    res = cur.execute(sql)
+    if res:
+         res = {'code':0,'msg':'update sucessfor'}
+    else:
+         res = {'code':1,'errmsg':'update failed'}
+    return res
+# 删除
+def delete(table,filed,data):
+    sql = "delete from %s where id = %s" %(table,data['id'])
+    res=cur.execute(sql)
+    if res:
+        res = {'code':0,'msg':'delete secussfor'}
+    else:
+        res = {'code':1,'msg':'delete failed'}
+    return res    
+
+
+
+
+
