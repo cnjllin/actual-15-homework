7adec958e5da949c7773abc984cf240383d51b69
root <root@kdc4.58os.org>
2017-09-16 22:43:32 +0800

    update

---
+++ eight/changhuawei/readme.py
@@ -0,0 +1,28 @@
+### 优化
+## 
+```
+.
+├── app.py
+├── readme.py
+├── static
+│   ├── css
+│   ├── img
+│   ├── js
+│   └── pulgin
+├── templates
+│   ├── base.html
+│   ├── index.html
+│   ├── information.html
+│   ├── login.html
+│   ├── reg.html
+│   ├── update.html
+│   └── userlist.html
+├── util.py
+└── util.pyc
+
+```
+
+![login](login.png)
+![userlist](index.png)
+![userlist](userlist.png)
+![userlist](update.png)
