2f9a2dc693f20a61ecf24b1e50b1a844b17af16a
dda163103 <dda1013@163.com>
2017-09-24 01:14:52 +0800

    luochuan homework

---
+++ nine/luochuan/db_config.py
@@ -0,0 +1,6 @@
+host="192.168.50.160"
+user="root"
+passwd="zhaoxf513"
+db="reboot15"
+port=3306
+charset="utf8"
---
+++ nine/luochuan/log_diaoyong.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+import logging
+
+def InitLogger(filename,level,name):
+    # create a logging object
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    # format log
+    formatter = logging.Formatter('%(asctime)s  %(name)s - %(levelname)s - %(filename)s -[line:%(lineno)s] - %(message)s')
+
+    # create the logging file handler and format the log file
+    fh = logging.FileHandler(filename,mode = 'a+')
+    fh.setFormatter(formatter)
+
+    # logger object load the handler
+    logger.addHandler(fh)
+
+    return logger
+
+
