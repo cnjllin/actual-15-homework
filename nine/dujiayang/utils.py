8a5e6b2625d7853b5b76094ce3889873dce51f9f
handishendu <handishendu@reboot.cn>
2017-09-24 01:14:34 +0800

    nine

--- nine/dujiayang/utils.py
+++ nine/dujiayang/utils.py
@@ -11,6 +11,7 @@ cur = conn.cursor()
 def insert(table,field,data):
     sql = "insert into %s (%s) values (%s)"  % (table,','.join(field),','.join(['"%s"' % data[x] for x in field]))
     res = cur.execute(sql)
+    util.WriteLog('[utils.py-insert插入数据库]').info(sql) 
     if res:
         result = {'code':0,'msg':'insert ok'}
     else:
@@ -20,7 +21,7 @@ def insert(table,field,data):
 def getlist(table,field):
     sql = "select * from %s" % table
     cur.execute(sql)
-    util.WriteLog('hehehe').info('123')
+    util.WriteLog('[utils.py-getlist]').info(sql) 
     res = cur.fetchall()
     if res:
        user = [{k:row[i] for i,k in enumerate(field)} for row in res]
@@ -37,6 +38,7 @@ def getone(table,field,data):
          sql = 'select * from %s where id="%s"' % (table,data['id'])
     print sql
     cur.execute(sql)
+    util.WriteLog('[utils.py-getone]').info(sql) 
     res = cur.fetchone()
     if res:
        user = {k:res[i] for i,k in enumerate(field)}
@@ -51,12 +53,27 @@ def update(table,field,data):
     sql = "update %s set %s where id=%s" %(table,','.join(conditions),data['id'])
     print sql
     res = cur.execute(sql)
+    util.WriteLog('[utils.py-update]').info(sql) 
     if res :
         result = {'code':0,'msg':'update ok'}
     else:
         result = {'code':1,'errmsg':'update fail'}
     return result 
 
-
+# 删除数据
+def delete(table,uid):
+        sql = "delete from %s where id = %s" % (table,uid)
+        print "删除用户sql"
+        print sql
+        res = cur.execute(sql)
+        if res:
+                result = {'code':0,'msg':'delete ok'}
+		util.WriteLog('[删除用户]').info('成功成功成功')
+        else:
+		util.WriteLog('[删除用户]').info('失败失败失败')
+                result = {'code':1,'msg':'delete false'}
+        print "util_delete_result"
+        print result['msg']
+        return result
 
    
